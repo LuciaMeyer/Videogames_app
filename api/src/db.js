05302e38aaf3ae64d8390bc6e1fdbfcdd7c58f58
LuciaM <luciameyer1603gmail.com>
2022-10-27 15:20:32 -0300

    deploy back 1

--- api/src/db.js
+++ api/src/db.js
@@ -2,10 +2,7 @@ require('dotenv').config(); // librería para usar .env
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
-
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 // const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
 //   logging: false, // set to console.log to see the raw SQL queries
 //   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
