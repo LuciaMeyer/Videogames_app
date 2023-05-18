c2710651e6130df57b49e2078fa919b0a01339d7
lucia <luciameyer1603@gmail.com>
2023-05-18 15:56:38 -0300

    img loader

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import emoji from '../../img/emoji.png'
 import './Loading.css'
 
 export const Loading = () => {
@@ -13,15 +14,16 @@ export const Loading = () => {
 
     return (
         <div className="loader">
-            <h3 className="h3loader">loading...</h3>
+            <h3 className="h3loader"></h3>
             <div className="spinner"></div>
             {!!time &&
                 <>
-                    <span className="textLoader">NO TE DESESPERES!</span>
+                    <span className="textLoader">loading...<br />NO TE DESESPERES!</span>
+                    <img className="imgLoader" src={emoji} />
                     <span className="text1Loader">
                     Esta aplicación está alojada en un hosting gratuito... los tiempos de carga son un poco más lentos.<br />
                     Te invito a esperar unos segundos más para descubrir el funcionamiento de la app!!
-                    </span>
+                    </span>                    
                 </>
             }
         </div>
